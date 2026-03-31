6d0b71f54f16613c233a000e0b02cf98ed47e846
Sajlendra Pandey <sajlendrapandey2022@gmail.com>
2026-03-31 22:29:50 +0530

    Added subarray sum K solution using prefix sum

--- Array/prefix_sum/1.py
+++ Array/prefix_sum/1.py
@@ -11,11 +11,43 @@
 
 
 # Q 2 --> Find sum of entire array using prefix
-def prefix_sum(arr):
-    prefix=[0]*len(arr)
-    prefix[0]=arr[0]
-    for i in range(1,len(arr)):
-        prefix[i]=prefix[i-1]+arr[i]
-    return prefix[-1]
-arr=[1,2,3]
-print(prefix_sum(arr))
+# def prefix_sum(arr):
+#     prefix=[0]*len(arr)
+#     prefix[0]=arr[0]
+#     for i in range(1,len(arr)):
+#         prefix[i]=prefix[i-1]+arr[i]
+#     return prefix[-1]
+# arr=[1,2,3]
+# print(prefix_sum(arr))
+
+
+# Q 3 --> Count total even numbers using prefix idea
+# def count_even(arr):
+#     prefix=[0]*len(arr)
+#     count=0
+#     prefix[0]= 1 if arr[0]%2==0 else 0
+#     for i in range(1,len(arr)):
+#         if arr[i]%2==0:
+#             prefix[i]=prefix[i-1]+1
+#         else:
+#             prefix[i]=prefix[i-1]
+#     return prefix[-1]
+# arr=[2,3,4,5,6,7,8]
+# p=(count_even(arr))
+# print(p)
+
+
+
+# Q  4 -->Find number of subarrays with sum = K (basic version)
+def subarray(arr,k):
+        count=0
+        for i in range(len(arr)):
+            s=0
+            for j in range(i,len(arr)):
+                s+=arr[j]
+                if s==k:
+                    count+=1
+        return count
+arr=[2,3,2,3]
+k=5
+print(subarray(arr,k))
