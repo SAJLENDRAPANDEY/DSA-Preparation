33ae153bc70b5d92d31818cb94a5f31033b5f7be
Sajlendra Pandey <sajlendrapandey2022@gmail.com>
2026-04-08 18:14:25 +0530

    Updated binary search code

--- Array/binary search/1.py
+++ Array/binary search/1.py
@@ -64,20 +64,58 @@
 
 
 # Q 4 --> Last occurence
-def last_occurence(arr,target):
-    start=0
-    stop=len(arr)-1
-    ans=-1
-    while start<=stop:
-        mid=(start+stop)//2
-        if arr[mid]==target:
-            ans=mid
-            start=mid+1
-        elif arr[mid]<target:
-            start=mid+1
-        else:
-            stop=mid-1
-    return ans
-arr = [1,2,2,2,3]
-target = 2
-print(last_occurence(arr,target))
+# def last_occurence(arr,target):
+#     start=0
+#     stop=len(arr)-1
+#     ans=-1
+#     while start<=stop:
+#         mid=(start+stop)//2
+#         if arr[mid]==target:
+#             ans=mid
+#             start=mid+1
+#         elif arr[mid]<target:
+#             start=mid+1
+#         else:
+#             stop=mid-1
+#     return ans
+# arr = [1,2,2,2,3]
+# target = 2
+# print(last_occurence(arr,target))
+
+
+
+
+
+# Q 5 --> Search Insert Position (LeetCode 35
+# def insert_position(arr,target):
+#     start=0
+#     stop=len(arr)-1
+#     while start<=stop:
+#         mid=(start+stop)//2
+#         if arr[mid]==target:
+#             return mid
+#         elif arr[mid]<target:
+#             start=mid+1
+#         else:
+#             stop=mid-1
+#     return start
+# arr = [1,3,5,6]
+# target = 5
+# print(insert_position(arr,target))
+
+
+
+# Q 6 --> first and last occurence (leetcode 34)
+def first_last(arr,target):
+    first=-1
+    last=-1
+    for i in range(len(arr)):
+        if arr[i]==target:
+            if first==-1:
+                first=i
+            last=i
+            
+    return [first,last]
+arr = [5,7,7,8,8,10]
+target = 8
+print(first_last(arr,target))
