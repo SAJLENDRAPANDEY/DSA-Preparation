733ae1412299b2198ff3bc7d4ce5fcc7c4aaf293
Sajlendra Pandey <sajlendrapandey2022@gmail.com>
2026-04-22 14:06:55 +0530

    Add reverse string using two pointers

--- String/Two pointer.py
+++ String/Two pointer.py
@@ -1,12 +1,25 @@
-# Palindrome
-def is_palindrome(s):
+# # Palindrome
+# def is_palindrome(s):
+#     left=0
+#     right=len(s)-1
+#     while left<=right:
+#         if s[left]!=s[right]:
+#             return False
+#         left+=1
+#         right-=1
+#     return True
+# s="abac"
+# print(is_palindrome(s))
+
+# Q 2-->Reverse string
+def reverse_string(s):
+    s=list(s)
     left=0
     right=len(s)-1
     while left<=right:
-        if s[left]!=s[right]:
-            return False
+        s[right],s[left]=s[left],s[right]
         left+=1
         right-=1
-    return True
-s="abac"
-print(is_palindrome(s))
+    return "".join(s)
+s="abc"
+print(reverse_string(s))
