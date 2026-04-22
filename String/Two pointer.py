e36d11b2b60a8560a5d81b4651de9832fa8c79ab
Sajlendra Pandey <sajlendrapandey2022@gmail.com>
2026-04-22 13:56:01 +0530

    Added two pointer string solution

---
+++ String/Two pointer.py
@@ -0,0 +1,12 @@
+# Palindrome
+def is_palindrome(s):
+    left=0
+    right=len(s)-1
+    while left<=right:
+        if s[left]!=s[right]:
+            return False
+        left+=1
+        right-=1
+    return True
+s="abac"
+print(is_palindrome(s))
