bb35bb32a7d67e8fcf528c407493ae63c0f63ece
Sajlendra Pandey <sajlendrapandey2022@gmail.com>
2026-04-22 14:18:06 +0530

    Add remove duplicates from string using two pointers

--- String/Two pointer.py
+++ String/Two pointer.py
@@ -12,14 +12,28 @@
 # print(is_palindrome(s))
 
 # Q 2-->Reverse string
-def reverse_string(s):
+# def reverse_string(s):
+#     s=list(s)
+#     left=0
+#     right=len(s)-1
+#     while left<=right:
+#         s[right],s[left]=s[left],s[right]
+#         left+=1
+#         right-=1
+#     return "".join(s)
+# s="abc"
+# print(reverse_string(s))
+
+
+# Q 3 --> Remove duplicates
+def remove_duplicate(s):
     s=list(s)
-    left=0
-    right=len(s)-1
-    while left<=right:
-        s[right],s[left]=s[left],s[right]
-        left+=1
-        right-=1
-    return "".join(s)
-s="abc"
-print(reverse_string(s))
+    slow=0
+    for fast in range(len(s)):
+        if s[fast]!=s[slow]:
+            slow+=1
+            s[slow]=s[fast]
+    return "".join(s[:slow+1])
+s="abbaaa"
+print(remove_duplicate(s))
+            
