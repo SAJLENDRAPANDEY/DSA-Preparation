bc98aad3b93b3b78d3b31defb3c49b6934d46508
Sajlendra Pandey <sajlendrapandey2022@gmail.com>
2026-04-24 13:13:24 +0530

    Added Array DSA solutions

--- Array/hashmap/1.py
+++ Array/hashmap/1.py
@@ -1,91 +1,106 @@
-# # Count frequency of elements
-# def count_freq(arr):
-#     freq={}
-#     for num in arr:
-#         freq[num]=freq.get(num,0)+1
-#     return freq
-# arr=[1,2,1,1,1,2,2,3,3,4,3,2]
-# print(count_freq(arr))
+# # # Count frequency of elements
+# # def count_freq(arr):
+# #     freq={}
+# #     for num in arr:
+# #         freq[num]=freq.get(num,0)+1
+# #     return freq
+# # arr=[1,2,1,1,1,2,2,3,3,4,3,2]
+# # print(count_freq(arr))
 
 
-# 2 Find duplicate element
-# def find_duplicate(arr):
-#     freq={}
-#     res=[]
-#     for num in arr:
-#         freq[num]=freq.get(num,0)+1
-#     for num in freq:
-#         if freq[num]>=2:
-#             res.append(freq[num])
-#     return res
-# arr=[1,1,1,2,2,1,2,3,4,4,3,6]
-# print(find_duplicate(arr))
+# # 2 Find duplicate element
+# # def find_duplicate(arr):
+# #     freq={}
+# #     res=[]
+# #     for num in arr:
+# #         freq[num]=freq.get(num,0)+1
+# #     for num in freq:
+# #         if freq[num]>=2:
+# #             res.append(freq[num])
+# #     return res
+# # arr=[1,1,1,2,2,1,2,3,4,4,3,6]
+# # print(find_duplicate(arr))
 
 
 
-# 3 --> First repeating element
-# def first_repeat(arr):
-#     n=len(arr)
-#     for i in range(n):
-#         for j in range(i+1,n):
-#             if arr[i]==arr[j]:
-#                 return i+1
+# # 3 --> First repeating element
+# # def first_repeat(arr):
+# #     n=len(arr)
+# #     for i in range(n):
+# #         for j in range(i+1,n):
+# #             if arr[i]==arr[j]:
+# #                 return i+1
             
-#     return -1
+# #     return -1
     
-# arr=[1,2,3,4,2]
-# print(first_repeat(arr))
+# # arr=[1,2,3,4,2]
+# # print(first_repeat(arr))
 
 
-# 4-->Two Sum (leetcode 1)
-# def two_sum(arr,target):
-#     seen={}
-#     for i in range(len(arr)):
-#         complement=target-arr[i]
+# # 4-->Two Sum (leetcode 1)
+# # def two_sum(arr,target):
+# #     seen={}
+# #     for i in range(len(arr)):
+# #         complement=target-arr[i]
 
-#         if complement in seen:
-#             return (seen[complement],i)
+# #         if complement in seen:
+# #             return (seen[complement],i)
     
-#         seen[arr[i]]=i
-# arr=[2,3,4,2,4]
-# target=5
-# print(two_sum(arr,target))
+# #         seen[arr[i]]=i
+# # arr=[2,3,4,2,4]
+# # target=5
+# # print(two_sum(arr,target))
 
 
-# 5 -->Majority Element(leetcode 169)
-# def majority_ele(arr):
-#     n=len(arr)
-#     seen={}
-#     for num in arr:
-#         seen[num]=seen.get(num,0)+1
+# # 5 -->Majority Element(leetcode 169)
+# # def majority_ele(arr):
+# #     n=len(arr)
+# #     seen={}
+# #     for num in arr:
+# #         seen[num]=seen.get(num,0)+1
 
-#         if seen[num]>n//2:
-#             return num
-# arr=[2,2,1,1,1,2,2]
-# print(majority_ele(arr))
+# #         if seen[num]>n//2:
+# #             return num
+# # arr=[2,2,1,1,1,2,2]
+# # print(majority_ele(arr))
 
 
-# 6-->
-def longestConsecutive( nums):
+# # 6-->
+# def longestConsecutive( nums):
         
-    if not nums:
-        return 0
-    num_Set=set(nums)
-    longest=0
+#     if not nums:
+#         return 0
+#     num_Set=set(nums)
+#     longest=0
 
-    for num in num_Set:
-        if num-1 not in num_Set:
-            current_num=num
-            count=1
+#     for num in num_Set:
+#         if num-1 not in num_Set:
+#             current_num=num
+#             count=1
 
-            while current_num+1 in num_Set:
-                current_num+=1
-                count+=1
-        longest=max(longest,count)
-    return longest
-
-
+#             while current_num+1 in num_Set:
+#                 current_num+=1
+#                 count+=1
+#         longest=max(longest,count)
+#     return longest
 
 
-nums=[100,4,200,1,3,2]
-print(longestConsecutive(nums))
+
+
+# nums=[100,4,200,1,3,2]
+# print(longestConsecutive(nums))
+
+
+
+
+# Q 10 --> First non repeting character
+def nonRepeatingChar(s):
+        #code here
+        freq={}
+        for ch in s:
+            freq[ch]=freq.get(ch,0)+1
+            
+            if freq[ch]==1:
+
+                return ch
+print(nonRepeatingChar("aabbacddddd"))
--- Array/two pointer/3Sum.py
+++ Array/two pointer/3Sum.py
@@ -1,29 +1,42 @@
-def max_water(arr):
-    n = len(arr)
-    if n == 0:
-        return 0
+# def max_water(arr):
+#     n = len(arr)
+#     if n == 0:
+#         return 0
 
-    left_max = [0] * n
-    right_max = [0] * n
+#     left_max = [0] * n
+#     right_max = [0] * n
 
-    # Fill left_max
-    left_max[0] = arr[0]
-    for i in range(1, n):
-        left_max[i] = max(arr[i], left_max[i - 1])
+#     # Fill left_max
+#     left_max[0] = arr[0]
+#     for i in range(1, n):
+#         left_max[i] = max(arr[i], left_max[i - 1])
 
-    # Fill right_max
-    right_max[n - 1] = arr[n - 1]
-    for i in range(n - 2, -1, -1):
-        right_max[i] = max(arr[i], right_max[i + 1])
+#     # Fill right_max
+#     right_max[n - 1] = arr[n - 1]
+#     for i in range(n - 2, -1, -1):
+#         right_max[i] = max(arr[i], right_max[i + 1])
 
-    # Calculate water
-    res = 0
-    for i in range(n):
-        res += min(left_max[i], right_max[i]) - arr[i]
+#     # Calculate water
+#     res = 0
+#     for i in range(n):
+#         res += min(left_max[i], right_max[i]) - arr[i]
 
-    return res
+#     return res
 
 
-# Driver code
-arr = [2, 1, 5, 3, 1, 0, 4]
-print(max_water(arr))  # Output: 9
+# # Driver code
+# arr = [2, 1, 5, 3, 1, 0, 4]
+# print(max_water(arr))  # Output: 9
+
+
+
+#  Q --ispower
+
+def isPower(x, y):
+        # code here
+    if x**2==y:
+        return True
+    return False
+x=2
+y=8
+print(isPower(x,y))
