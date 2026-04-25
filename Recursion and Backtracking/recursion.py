61e6ba7382a6e6d6026f15911add6be6d9291c45
Sajlendra Pandey <sajlendrapandey2022@gmail.com>
2026-04-25 10:58:00 +0530

    Added Recursion and Backtracking (basic problems)

--- Recursion and Backtracking/recursion.py
+++ Recursion and Backtracking/recursion.py
@@ -1,7 +1,16 @@
-# Print 1 to N
-def print_1(n):
-    if n==0 :
+# # Print 1 to N
+# def print_1(n):
+#     if n==0 :
+#         return 
+#     print_1(n-1)
+#     print(n,end=" ")
+# print(print_1(5))
+
+# q 2-->Print N to 1
+def print_n_1(n):
+    if n==0:
         return 
-    print_1(n-1)
     print(n,end=" ")
-print(print_1(5))
+    print_n_1(n-1)
+    
+print(print_n_1(5))
