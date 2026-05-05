619e4c7cedce0c3661e4e10656a39c3dca5ee303
Sajlendra Pandey <sajlendrapandey2022@gmail.com>
2026-05-06 00:42:02 +0530

    Updated stack logic

--- stack/stack.py
+++ stack/stack.py
@@ -12,12 +12,21 @@
 
 
 # Question 1 --> String ko reverse karo using stack
-def reverse_string(s):
-    stack=[]
-    for ch in s:
-        stack.append(ch)
-    res=""
-    while stack:
-        res+=(stack.pop())
-    return res
-print(reverse_string("Hello"))
+# def reverse_string(s):
+#     stack=[]
+#     for ch in s:
+#         stack.append(ch)
+#     res=""
+#     while stack:
+#         res+=(stack.pop())
+#     return res
+# print(reverse_string("Hello"))
+
+
+# Question 2 --> Check stack empty or not
+stack=[2]
+def check_empty(stack):
+    return len(stack)==0
+        
+    
+print(check_empty(stack))
