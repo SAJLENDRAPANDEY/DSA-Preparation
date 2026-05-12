26b9219fd936d82d2632312a7039abd2ee680343
Sajlendra Pandey <sajlendrapandey2022@gmail.com>
2026-05-12 12:07:43 +0530

    Updated queue operations code

--- Queue/1.py
+++ Queue/1.py
@@ -5,6 +5,25 @@ q.append(2)
 q.append(3)
 q.append(3)
 
-print(q.popleft())
+# print(q.popleft())
 
-print(q)
+# print(q)
+
+#  q  1 --> Print Front Element
+print("Front element :",q[0])
+
+# q 2 --> Print Rear Element
+print("Rear elemenr:",q[-1])
+
+# Q 3 -->Check Queue Empty
+def check_queue(q):
+    
+    
+    if len(q)==0:
+        return "Empty"
+    else:
+        return "Not empty"
+    
+q=deque()
+q.append(2)
+print(check_queue(q))
