3a4b90294b4da75f637a1f8aa7a867cc2ffa4ca5
Sajlendra Pandey <sajlendrapandey2022@gmail.com>
2026-06-17 09:55:23 +0530

    update valid pali code

---
+++ revesion dsa/arr2.py
@@ -0,0 +1,20 @@
+# q 1 --> Check valid palindrome
+
+def valid_pali(s):
+    left=0
+    right=len(s)-1
+    while left<=right:
+        if not s[left].isalnum():
+            left+=1
+            continue
+        if not s[right].isalnum():
+            right-=1
+            continue
+        if s[left].lower()!=s[right].lower():
+            return False
+        
+        left+=1
+        right-=1
+    return True
+s="A man, a plan, a canal: Panama d"
+print(valid_pali(s))
