f1ce859b8c9eca2ea44ea47da96c8ccff31d06e6
Sajlendra Pandey <sajlendrapandey2022@gmail.com>
2026-06-17 10:11:59 +0530

    update reve_str code

--- revesion dsa/arr2.py
+++ revesion dsa/arr2.py
@@ -1,37 +1,50 @@
-# # q 1 --> Check valid palindrome
+# # # q 1 --> Check valid palindrome
 
-# def valid_pali(s):
-#     left=0
-#     right=len(s)-1
-#     while left<=right:
-#         if not s[left].isalnum():
-#             left+=1
-#             continue
-#         if not s[right].isalnum():
-#             right-=1
-#             continue
-#         if s[left].lower()!=s[right].lower():
-#             return False
+# # def valid_pali(s):
+# #     left=0
+# #     right=len(s)-1
+# #     while left<=right:
+# #         if not s[left].isalnum():
+# #             left+=1
+# #             continue
+# #         if not s[right].isalnum():
+# #             right-=1
+# #             continue
+# #         if s[left].lower()!=s[right].lower():
+# #             return False
         
-#         left+=1
-#         right-=1
-#     return True
-# s="A man, a plan, a canal: Panama "
-# print(valid_pali(s))
+# #         left+=1
+# #         right-=1
+# #     return True
+# # s="A man, a plan, a canal: Panama "
+# # print(valid_pali(s))
 
 
-# two sum (lc == 167)
-def two_sum(arr,target):
+# # two sum (lc == 167)
+# def two_sum(arr,target):
+#     left=0
+#     right=len(arr)-1
+#     while left<=right:
+#         s=arr[left]+arr[right]
+#         if s==target:
+#             return [arr[left],arr[right]]
+#         elif s<target:
+#             left+=1
+#         else:
+#             right-=1
+# arr=[2,4,5,6]
+# target=10
+# print(two_sum(arr,target))
+
+
+# reverse_string(lc-344)
+def reve_str(s):
     left=0
-    right=len(arr)-1
+    right=len(s)-1
     while left<=right:
-        s=arr[left]+arr[right]
-        if s==target:
-            return [arr[left],arr[right]]
-        elif s<target:
-            left+=1
-        else:
-            right-=1
-arr=[2,4,5,6]
-target=10
-print(two_sum(arr,target))
+        s[left],s[right]=s[right],s[left]
+        left+=1
+        right-=1
+    return s
+s=["h","e","l","l","o"]
+print(reve_str(s))
