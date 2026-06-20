da50f2e22cbb8b0b3fa01c86cf4336ca44152713
Sajlendra Pandey <sajlendrapandey2022@gmail.com>
2026-06-20 10:21:14 +0530

    update max sum code

--- revesion dsa/arr2.py
+++ revesion dsa/arr2.py
@@ -63,15 +63,40 @@
 
 
 # Q 3 --> Maximum Average Subarray I (lc(643))
-def max_arr(arr,k):
-    n=len(arr)
+# def max_arr(arr,k):
+#     n=len(arr)
+#     window_sum=sum(arr[:k])
+#     max_sum=window_sum
+#     for i in range(n-k):
+#         window_sum=(window_sum-arr[i]+arr[i+k])
+#         max_sum=max(max_sum,window_sum)
+#     return (float(max_sum)/k)
+# arr=[1,12,-5,-6,50,3]
+# k = 4
+
+# print(max_arr(arr,k))
+
+
+
+
+
+
+
+
+
+
+
+
+
+# or 
+def max_sum(arr,k):
     window_sum=sum(arr[:k])
     max_sum=window_sum
+    n=len(arr)
     for i in range(n-k):
-        window_sum=(window_sum-arr[i]+arr[i+k])
+        window_sum=window_sum-arr[i]+arr[i+k]
         max_sum=max(max_sum,window_sum)
-    return (float(max_sum)/k)
-arr=[1,12,-5,-6,50,3]
-k = 4
-
-print(max_arr(arr,k))
+    return max_sum
+arr=[2,3,4,5,2,1]
+k=3
+print(max_sum(arr,k))
