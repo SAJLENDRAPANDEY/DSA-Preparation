f1e9bf3fe9314c6d6b5106de35d7a642b5f75cc2
Sajlendra Pandey <sajlendrapandey2022@gmail.com>
2026-08-17 11:45:10 +0530

    update code

--- revesion dsa/linked_list.py
+++ revesion dsa/linked_list.py
@@ -18,3 +18,19 @@ l1.head=Node(10)
 l1.head.next=Node(20)
 l1.head.next.next=Node(30)
 l1.display()
+
+
+# insert at beginging
+new_node=Node(5)
+# new_node.next=l1.head
+# l1.head=new_node
+
+# l1.display()
+
+
+# insert at the end
+curr=l1.head
+while curr.next is not None:
+    curr=curr.next
+curr.next=Node(50)
+l1.display()
