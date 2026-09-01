b5891651d3137ec9e0dde900711ad88f5d6be49c
Sajlendra Pandey <sajlendrapandey2022@gmail.com>
2026-09-01 11:11:27 +0530

    update code

--- revesion dsa/stack.py
+++ revesion dsa/stack.py
@@ -5,6 +5,19 @@ class Stack():
         element=int(input("Enter the number :"))
         self.stack.append(element)
         print("Element push to stack is :",element)
+
+    def pop(self):
+        if not self.stack:
+            print("Stack is Empty ")
+            return
+        ele=self.stack.pop()
+        print("Element pop:",ele)
+
+
 s=Stack()
 s.push()
+
+print(s.stack)
+
+s.pop()
 print(s.stack)
